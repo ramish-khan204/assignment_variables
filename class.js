ea8da10f1208e5de9fae1349fd4360b4565fa9a8
ramish-khan204 <159920981+ramish-khan204@users.noreply.github.com>
2024-03-04 00:15:14 +0500

    Add files via upload
    Typescript assignment using variables

---
+++ class.js
@@ -0,0 +1,32 @@
+// personal info
+var personName = "Ramish";
+console.log(personName);
+var age = 17;
+console.log(age);
+var isstudent = true;
+console.log(isstudent);
+var religion = "Islam";
+console.log(religion);
+var isemploy = false;
+console.log(isemploy);
+// Address
+var address = "xyz.Block D";
+console.log(address);
+var area = "unit#2.Latifabad";
+console.log(area);
+var cityName = "Hyderabad";
+console.log(cityName);
+var country = "Pakistan";
+console.log(country);
+//...............
+var favroitColour = "Black";
+console.log(favroitColour);
+favroitColour = "Lilac grey";
+console.log(favroitColour);
+//
+var randomvalue = "https://github.com/ramish-khan204";
+console.log(randomvalue);
+var result = undefined;
+console.log(result);
+var sign = null;
+console.log(null);
