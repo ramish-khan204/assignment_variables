c27cc2e01f4538955ed983fcf824f3a25a9dc0e1
ramish-khan204 <159920981+ramish-khan204@users.noreply.github.com>
2024-03-22 17:01:15 +0500

    Add files via upload

---
+++ class.ts
@@ -0,0 +1,33 @@
+// personal info
+let personName: string= "Ramish";
+console.log(personName);
+let age:number= 17;
+console.log(age);
+let isstudent: boolean= true;
+console.log(isstudent);
+const religion:string= "Islam";
+console.log(religion);
+let isemploy: boolean=false;
+console.log(isemploy);
+// Address
+let address: string="xyz.Block D";
+console.log(address);
+let area:string="unit#2.Latifabad";
+console.log(area);
+let cityName: string= "Hyderabad";
+console.log(cityName);
+let country:string="Pakistan";
+console.log(country);
+//...............
+let favroitColour: string= "Black";
+console.log(favroitColour);
+favroitColour = "Lilac grey";
+console.log(favroitColour);
+//
+let randomvalue: any="https://github.com/ramish-khan204";
+console.log(randomvalue);
+let result: undefined= undefined;
+console.log(result);
+let sign:null= null;
+console.log(null);
+
