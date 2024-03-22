a8b9dc209db86d1653a10dc1f2504d542e4d145e
ramish-khan204 <159920981+ramish-khan204@users.noreply.github.com>
2024-03-22 16:44:20 +0500

    Add files via upload

---
+++ class.ts
@@ -0,0 +1,10 @@
+let name1: string="ramish";
+let age: number= 17
+let isMarried: boolean = false
+//let abc: undefined= undefined
+//let xyz: null=null
+//let pi = 3.14
+//pi=7
+
+const pi = 3.142
+//pi =10
